ee3d38d5094bcefb0a995cc0b142b56f7e1e7d39
Yash <aminyash8@gmail.com>
2020-05-17 19:50:09 +0530

    Crawler

--- main.go
+++ main.go
@@ -6,8 +6,38 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
+	"sync"
 )
+
+// Flag inputs
+var crawlIncludePathRegexString = "test"
+var crawlExcludePathRegexString = "^a" // match nothing
+var downloadIncludePathRegexString = ".*"
+var downloadExcludePathRegexString = "^a" // match nothing
+var downloadExtensionsRegexString = "png"
+var outputDir = "test/output"
+var download = true
+
+// Todo: status code
+// Todo: mimetype
+
+// Regex of flags
+var crawlIncludePathRegex = regexp.MustCompile(crawlIncludePathRegexString)
+var crawlExcludePathRegex = regexp.MustCompile(crawlExcludePathRegexString)
+var downloadExtensionsRegex = regexp.MustCompile(downloadExtensionsRegexString)
+var downloadIncludePathRegex = regexp.MustCompile(downloadIncludePathRegexString)
+var downloadExcludePathRegex = regexp.MustCompile(downloadExcludePathRegexString)
+
+// WaitGroups for crawler
+var urlsWG sync.WaitGroup
+var crawlerWG sync.WaitGroup
+
+// To store processed URLs so we don't send requests to them again
+var crawlerProcessedURLs []string
 
 func getAbsoluteURL(base string, path string) string {
 	abs, err := url.Parse(base)
@@ -23,6 +53,7 @@ func getAbsoluteURL(base string, path string) string {
 	// fmt.Println(abs.ResolveReference(u).String())
 	return abs.ResolveReference(u).String()
 }
+
 func getAttributeOfElement(node *html.Node, tag string, attribute string, values *[]string) {
 	var scraper func(*html.Node)
 	attribute = strings.ToLower(attribute) // ??? should convert to lowercase or not?
@@ -42,6 +73,7 @@ func getAttributeOfElement(node *html.Node, tag string, attribute string, values
 	}
 	scraper(node)
 }
+
 func getLinks(pageURL string, res string) []string {
 	doc, err := html.Parse(strings.NewReader(res))
 	var values []string
@@ -57,7 +89,73 @@ func getLinks(pageURL string, res string) []string {
 	return values
 }
 
-func process(url string) {
+// If URL is not processed yet, add it to channel
+func addURL(c chan string, url string) {
+	for _, processedURL := range crawlerProcessedURLs {
+		if processedURL == url {
+			urlsWG.Done()
+			// fmt.Println("\t already done\t:", url)
+			return
+		}
+	}
+	crawlerProcessedURLs = append(crawlerProcessedURLs, url)
+	c <- url
+	fmt.Println("\t new link\t:", url)
+}
+
+// Returns URL path, without hostname or query string
+func getURLPath(_url string) string {
+	parsed, err := url.Parse(_url)
+	if err != nil {
+		return ""
+	}
+	return parsed.Path
+}
+
+// Matches crawl-regex
+func shouldCrawl(url string) bool {
+	urlBytes := []byte(url)
+	return crawlIncludePathRegex.Match(urlBytes) &&
+		!crawlExcludePathRegex.Match(urlBytes)
+}
+
+// If download flag is enabled, matches URL with download-regex
+func shouldSave(path string) bool {
+	if !download {
+		return false
+	}
+	split := strings.SplitN(path, ".", -1)
+	ext := split[len(split)-1]
+	if len(split) == 0 {
+		ext = ""
+	}
+
+	return downloadIncludePathRegex.Match([]byte(path)) &&
+		downloadExtensionsRegex.Match([]byte(ext))
+}
+
+// Creates directories, and writes to file
+func save(path string, data []byte) {
+	path = outputDir + path
+	dirPath := filepath.Dir(path)
+	os.MkdirAll(dirPath, 0777)
+
+	f, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	_, err = f.Write(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func process(c chan string, url string) {
+	defer urlsWG.Done()
+
 	response, err := http.Get(url)
 	if err != nil {
 		return
@@ -72,25 +170,61 @@ func process(url string) {
 	// Check if Content-Type is html or not...
 	contentType := response.Header.Get("Content-Type")
 	isHTML := strings.Contains(contentType, "html")
+	// Some websites do not return Content-Type header in response
+	// So we have to parse every request
 
-	if !isHTML {
-		return
-	}
 	body := string(bytes)
-	var links = getLinks(url, body)
-	fmt.Println(body, "\n\n\n")
 
-	fmt.Println("[+] Links")
+	// Get all the URLs from current page, and add to
+	// channel
+	var links = getLinks(url, body)
 	for _, link := range links {
-		fmt.Println(" >", link)
+		if shouldSave(link) || shouldCrawl(link) {
+			urlsWG.Add(1)
+			go addURL(c, link)
+		}
 	}
 
-	_ = body
+	// Save current request body
+	currentURLPath := getURLPath(url)
+	if shouldSave(currentURLPath) {
+		save(currentURLPath, bytes)
+	}
+
+	// _ = body
 	_ = isHTML
+}
+
+// Crawler, takes URLs from channel
+func crawler(c chan string) {
+	defer crawlerWG.Done()
+
+	for url := range c {
+		fmt.Println("[+] Crawler: ", url)
+		process(c, url)
+	}
+}
+
+func startCrawler(url string) {
+	var connections = 20
+
+	c := make(chan string)
+
+	for i := 0; i < connections; i++ {
+		crawlerWG.Add(1)
+		go crawler(c)
+	}
+
+	urlsWG.Add(1)
+	go addURL(c, url)
+
+	urlsWG.Wait()
+	close(c)
+	crawlerWG.Wait()
 }
 
 func main() {
 	fmt.Println("--------------------------------------")
 	url := "http://localhost/"
-	process(url)
+	startCrawler(url)
 }
